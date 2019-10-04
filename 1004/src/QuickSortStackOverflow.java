4a5a475f3214f37fea767f5aa1cac56336a22098
dapaixing <53035807+dapaixing@users.noreply.github.com>
2019-10-04 22:26:13 +0800

    排序复习

---
+++ 1004/src/QuickSortStackOverflow.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class QuickSortStackOverflow {
+    public static void main(String[] args) {
+        int[] a = Lab.buildEquals(50000);
+        QuickSort.quickSort(a);
+    }
+}
