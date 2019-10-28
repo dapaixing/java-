0a90849ca64cc59b024c389539157dec741392b1
dapaixing <53035807+dapaixing@users.noreply.github.com>
2019-10-28 22:41:48 +0800

    线程

---
+++ 1028/线程安全/src/RunnableImpl.java
@@ -0,0 +1,13 @@
+public class RunnableImpl implements Runnable {
+    private int ticket = 100;
+
+    @Override
+    public void run() {
+        while (true) {
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + "正在卖第" + ticket + "张票");
+                ticket--;
+            }
+        }
+    }
+}
