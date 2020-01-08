859be422c9ae5f1c06fc36074ec62cb2dcce3f8a
dapaixing <53035807+dapaixing@users.noreply.github.com>
2020-01-08 22:32:19 +0800

    练习

---
+++ 学生信息管理系统/学生信息管理系统/源.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 15 //学生姓名和学号最大字符数为15，实际请更改
+#define N 200 //最大学生人数，实际请更改
+
+struct record
+{
+	char code[LEN + 1];//学号
+	char 
+};
