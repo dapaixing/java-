3f1a1152301d4e203cb10d45d75a1c8fec5c54a1
dapaixing <53035807+dapaixing@users.noreply.github.com>
2020-03-08 20:39:33 +0800

    算法练习

--- 练习/src/Main.java
+++ 练习/src/Main.java
@@ -332,19 +332,55 @@
 //}
 
 
+//import java.util.*;
+//import java.math.*;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        while(s.hasNext()){
+//            int n = s.nextInt();
+//            BigInteger[] bg = new BigInteger[n];
+//            for(int i=0; i<n; i++)
+//                bg[i] = s.nextBigInteger();
+//            Arrays.sort(bg);
+//            for(int i=0; i<n; i++)
+//                System.out.println(bg[i]);
+//        }
+//    }
+//}
+
 import java.util.*;
-import java.math.*;
-public class Main{
+
+public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        while(s.hasNext()){
-            int n = s.nextInt();
-            BigInteger[] bg = new BigInteger[n];
-            for(int i=0; i<n; i++)
-                bg[i] = s.nextBigInteger();
-            Arrays.sort(bg);
-            for(int i=0; i<n; i++)
-                System.out.println(bg[i]);
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        int count = 0;
+        while (sc.hasNextLine()) {
+            String str_1 = sc.nextLine();
+            mapDispose(map, str_1);
+        }
+        for (String st : map.keySet()) {
+            count++;
+            if (count > (map.size() - 8)) {
+                System.out.println(st + " " + map.get(st));
+            }
+        }
+    }
+
+    public static void mapDispose(Map<String, Integer> map, String str) {
+        String[] strArr = str.split("\\s+");
+        String[] nameArr = strArr[0].split("\\\\");
+        int num = Integer.parseInt(strArr[1]);
+        String name = nameArr[nameArr.length - 1];
+        if (name.length() > 16) {
+            name = name.substring(name.length() - 16);
+        }
+        String key = name + " " + num;
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + 1);
+        } else {
+            map.put(key, 1);
         }
     }
 }
